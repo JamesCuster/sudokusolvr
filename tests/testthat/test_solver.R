5fc23ac357cb71156b35d0f946e8b29e20f7e3e5
James Custer <jamescuster0121@gmail.com>
2019-05-06 15:39:13 -0500

    add initArray unit test

--- tests/testthat/test_solver.R
+++ tests/testthat/test_solver.R
@@ -1,5 +1,8 @@
 context("Testing the solver functions")
 
-test_that("upArray returns array", {
-
+test_that("initArray returns correct list object", {
+  expect_is(initArray(puzzle1), "list")
+  expect_is(initArray(puzzle1)[[1]], "matrix")
+  expect_is(initArray(puzzle1)[[2]], "array")
 })
+
