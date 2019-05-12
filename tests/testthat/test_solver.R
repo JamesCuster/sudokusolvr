021e6a3a40973a6f63dbdba97a2cc6d134290df5
James Custer <jamescuster0121@gmail.com>
2019-05-12 12:57:02 -0500

    update initArray tests

--- tests/testthat/test_solver.R
+++ tests/testthat/test_solver.R
@@ -3,11 +3,8 @@ context("Testing the solver functions")
 
 # Todo notes. I changed what initArray outputs, need to update test
 
-test_that("initArray returns correct list object", {
-  expect_is(initArray(puzzle1), "list")
-  expect_is(initArray(puzzle1)[[1]], "matrix")
-  expect_equal(dim(initArray(puzzle1)[[1]]), c(9, 9))
-  expect_is(initArray(puzzle1)[[2]], "array")
-  expect_is(dim(initArray(puzzle1)[[2]]), c(9, 9, 9))
+test_that("initArray returns 9x9x9 logical array", {
+  expect_is(initArray(puzzle1), "array")
+  expect_equal(typeof(initArray(puzzle1)), "logical")
+  expect_equal(dim(initArray(puzzle1)), c(9, 9, 9))
 })
-
