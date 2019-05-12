7076f41e8e77bf094206503a15c2f4d0aec7b1bb
James Custer <jamescuster0121@gmail.com>
2019-05-12 13:16:57 -0500

    add puzzle1 data documnetation

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' A 9x9 matrix representation of a sudoku puzzle
+#'
+#' a matrix object with 9 rows and 9 columns which is a representation of a
+#' sudoku puzzle. Cells in the soduku puzzle which do not contain a number are
+#' filled with \code{NA}.
+#'
+#' @format a matrix with 9 rows and 9 columns
+"puzzle1"
