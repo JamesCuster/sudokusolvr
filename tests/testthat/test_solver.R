c594715d01cee039278f7b725423a3c5818b2408
James Custer <jamescuster0121@gmail.com>
2019-05-13 04:01:28 -0500

    add/modify tests for sudokuPuzzle and initArray

--- tests/testthat/test_solver.R
+++ tests/testthat/test_solver.R
@@ -1,10 +1,21 @@
 context("Testing the solver functions")
 
 
-# Todo notes. I changed what initArray outputs, need to update test
+# sudokuPuzzle ------------------------------------------------------------
+
+test_that("sodukuPuzzle class constructor function", {
+  expect_is(sudokuPuzzle(puzzle1), "sudokuPuzzle")
+  expect_equal(sudokuPuzzle(puzzle1)$puzzle, puzzle1)
+  expect_equal(sudokuPuzzle(puzzle1)$array, puzzle1Array)
+})
+
+
+# initArray ---------------------------------------------------------------
 
 test_that("initArray returns 9x9x9 logical array", {
   expect_is(initArray(puzzle1), "array")
   expect_equal(typeof(initArray(puzzle1)), "logical")
   expect_equal(dim(initArray(puzzle1)), c(9, 9, 9))
+  expect_equal(initArray(puzzle1), puzzle1Array)
 })
+
