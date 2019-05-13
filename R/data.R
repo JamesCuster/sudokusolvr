c326feb27c7afad3c713dbfe663a2cc29cebf1b8
James Custer <jamescuster0121@gmail.com>
2019-05-13 03:40:37 -0500

    add puzzle1 array to data for testing

--- R/data.R
+++ R/data.R
@@ -6,3 +6,14 @@
 #'
 #' @format a matrix with 9 rows and 9 columns
 "puzzle1"
+
+#' A 9x9x9 array representing solutions of a sudoku puzzle
+#'
+#' a logical array object where the first and second dimensions represent the
+#' rows and columns of a sudoku puzzle and the third dimension represnts which
+#' numbers 1-9 can go in a particular row/column combination. Cells in the array
+#' in which it is unknown whether that number can go in the row/column
+#' combination are filled with \code{NA}
+#'
+#' @format a 9x9x9 array
+"puzzle1Array"
